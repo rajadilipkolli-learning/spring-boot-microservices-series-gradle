78698a0e91323ff1a4516f9a53c9c80cc0921bce
rajadilipkolli <rajakolli@deloitte.com>
2021-02-05 18:18:56 +0530

    adds order-service module

--- order-service/src/main/java/com/example/orderservice/config/WebMvcConfig.java
+++ order-service/src/main/java/com/example/orderservice/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.catalogservice.config;
+package com.example.orderservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
