8e7efb516f258c617cae007c54c1766b427c9b17
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2021-05-31 02:08:22 +0530

    Fix Build Issue (#18)
    * Bump org.springframework.boot from 2.4.2 to 2.5.0
    
    Bumps [org.springframework.boot](https://github.com/spring-projects/spring-boot) from 2.4.2 to 2.5.0.
    - [Release notes](https://github.com/spring-projects/spring-boot/releases)
    - [Commits](https://github.com/spring-projects/spring-boot/compare/v2.4.2...v2.5.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix Junits failure and polish
    
    * polish
    
    * fix order-service issues
    
    * fix build Issue
    
    * removes spotless plugin
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ catalog-service/src/main/java/com/example/catalog/config/ApplicationProperties.java
@@ -0,0 +1,10 @@
+package com.example.catalog.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties("application")
+public class ApplicationProperties {
+    private String app;
+}
