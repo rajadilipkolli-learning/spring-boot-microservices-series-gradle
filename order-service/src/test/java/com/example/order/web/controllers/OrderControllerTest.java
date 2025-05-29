386dbe712d9808f6a66ca48f340470051b516f5a
Raja Kolli <rajadilipkolli@gmail.com>
2025-05-29 15:16:05 +0000

    removes deprecated method

--- order-service/src/test/java/com/example/order/web/controllers/OrderControllerTest.java
+++ order-service/src/test/java/com/example/order/web/controllers/OrderControllerTest.java
@@ -24,9 +24,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = OrderController.class)
@@ -35,7 +35,7 @@ class OrderControllerTest {
 
     @Autowired private MockMvc mockMvc;
 
-    @MockBean private OrderService orderService;
+    @MockitoBean private OrderService orderService;
 
     @Autowired private ObjectMapper objectMapper;
 
