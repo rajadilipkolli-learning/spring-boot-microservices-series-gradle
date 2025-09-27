8f0687783187fcf8f83a171dd73d55e628ebbfa2
Raja Kolli <rajadilipkolli@gmail.com>
2025-09-27 15:32:51 +0530

    change postgresql version

--- catalog-service/src/test/java/com/example/catalog/common/ContainersConfig.java
+++ catalog-service/src/test/java/com/example/catalog/common/ContainersConfig.java
@@ -14,6 +14,6 @@ public class ContainersConfig {
     @ServiceConnection
     @RestartScope
     PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17.5-alpine"));
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:18.0-alpine"));
     }
 }
